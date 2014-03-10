bb946c9c720767a3c75970e42d57ddb5a7518474
James Percent <james@syndeticlogic.org>
2014-03-10 17:55:40 -0400

    Added Unlock's Windows dependencies

---
+++ unlock_deps.py
@@ -0,0 +1,11 @@
+
+numpy-MKL, scipy, matplotlib, ipython, pandas, sympy, nose and many of their dependencies (setuptools, dateutil,
+pytz, six, pillow, pygments, jinja2, markupsafe, statsmodels, pytables, numexpr, bottleneck, tornado,
+pyzmq, gmpy2, pyside, etc
+
+pip install numpy-MKL
+
+pip install MySQL_python==1.2.2
+
+James
+
